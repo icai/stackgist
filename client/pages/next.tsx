dde15bae08ddadbfd50e7694cf7fad0c87001ec1
Terry Cai <gidcai@gmail.com>
2018-10-12 00:34:02 +0800

    add sequelize package

--- client/pages/next.tsx
+++ client/pages/next.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-export default ()=>(<div><h1>hi, egg-nextjs</h1>
-   <style jsx global>{`
-      h1 {
-          color:red;
-      }
-  `}</style>
+export default ()=>(
+<div>
+    <h1>hi, next</h1>
 </div>)
