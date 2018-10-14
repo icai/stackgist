f1d1a5236e6ee9aa913b9f4134a4829ce7ecb21d
Terry Cai <gidcai@gmail.com>
2018-10-15 02:23:52 +0800

    tshelper

---
+++ tshelper.js
@@ -0,0 +1,14 @@
+module.exports = {
+    watchDirs: {
+      model: {
+        path: 'app/models', // dir path
+        // pattern: '**/*.(ts|js)', // glob pattern, default is **/*.(ts|js). it doesn't need to configure normally.
+        // generator: 'class', // generator name
+        // framework: 'larva', // framework name
+        // interface: 'IModel',  // interface name
+        // caseStyle: 'upper', // caseStyle for loader
+        // interfaceHandle: val => `ReturnType<typeof ${val}>`, // interfaceHandle
+        // trigger: ['add', 'unlink'], // recreate d.ts when receive these events, all events: ['add', 'unlink', 'change']
+      }
+    }
+  }
